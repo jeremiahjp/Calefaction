e0e07b015f928e73608cb256fde852e9b3c50ac6
jeremiahjp <i.r.stuff@gmail.com>
2020-08-13 21:29:59 -0500

    Oops
    Did that backwards.

--- utils/discordresponse.js
+++ utils/discordresponse.js
@@ -6,7 +6,7 @@ const DiscordResponse = (message) => {
     .setTitle(message.title)
     .setAuthor(`${message.author_title ? message.author_title : ''}`, `${message.author_icon ? message.author_icon : ''}`, ``)
     .setDescription(message.description)
-    .addFields(message.fields.length ?  {} : message.fields)
+    .addFields(message.fields.length ? message.fields : [])
     .setColor(message.color)
     .setImage(message.image)
     .setThumbnail(message.thumbnail)
